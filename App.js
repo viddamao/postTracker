e48ef0b382a4d13038eafb0adc5fda8dedb6f4a5
viddamao <viddamaowj@hotmail.com>
2018-04-01 18:44:56 -0400

    create project hieriachy
    fix dependency

--- App.js
+++ App.js
@@ -1,25 +1,4 @@
-import React from 'react';
-import { Button } from 'antd-mobile';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './src/index';
+console.disableYellowBox = true;
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button>antd-mobile button</Button>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
